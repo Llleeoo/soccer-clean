bb560e09e3d4c8e55ff314f8e659c9a19d9d0081
Llleeoo <120419742+Llleeoo@users.noreply.github.com>
2023-03-20 21:26:25 +0100

    Révision Théorie
    J'ai changé un petit truc à la défense je pense que ca fonctionnera mieux comme ça et j'ai aussi refait toute l'attaque, selon la même technique mais de façon plus clair et qui fonctionne à chaque fois. Je crois qu'on a toujours pas de shoot par contre ?
    Le rapport correspond en vrai à l'hypothénuse du triangle formé avec l'axe x, le centre des buts et la place voulu du robot.
    Il faudra peut-être revoir la longueur des lignes 29-30, le piège est que l'on ne peut pas diviser le terrain en 2 pour chacune des équipes car l'attaquant doit pouvoir réagir si le ballon est dans son côté du terrain... la seule différence entre le if et else est le - qui devient +.

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 from rsk import Client, ClientRobot, constants
 import numpy as np
-from math import sqrt, atan
+from math import sqrt, tan, cos, sin
 
 # All mesures are in meters
 ROBOT_RADIUS = 0.09
@@ -16,7 +16,7 @@ def main_loop():
     # Defensor move
     allies[0].goto((
         ((constants.field_width/2)-ROBOT_RADIUS)*X_MULT,
-        max(min(ball[1], -constants.goal_virtual_height/2), constants.goal_virtual_height/2),
+        max(min(ball[1], constants.goal_virtual_height/2), -constants.goal_virtual_height/2),
         DEFAULT_ANGLE
     ))
 
@@ -25,12 +25,13 @@ def main_loop():
     # (feat pithagore)
     # (feat trigonometrie)
     pose = allies[1].pose
-    pose[0] *= X_MULT
-    rapport = sqrt(ball[0]**2 + ball[1]**2) + ROBOT_RADIUS + BALL_RADIUS
+    Y_MULT = 1 if pose[1]>= 0 else -1
+    rapport = sqrt((constants.field_length/2 - ball[0]) ** 2 + ball[1] ** 2) + ROBOT_RADIUS + BALL_RADIUS if X_MULT == 1 else sqrt((constants.field_length/2 + ball[0]) ** 2 + ball[1] ** 2) + ROBOT_RADIUS + BALL_RADIUS
+    angle = tan(ball[1] ** 2 / (constants.field_length/2 - ball[0]) ** 2) if X_MULT == 1 else tan(ball[1] ** 2 / (constants.field_length/2 + ball[0]) ** 2)
     destination = (
-        ball[0] * rapport * X_MULT,
-        -abs(ball[1] * rapport),
-        atan(ball[1]/ball[0])
+        rapport * cos(angle) * X_MULT,
+        rapport * sin(angle) * Y_MULT,
+        angle * np.pi / 180 * -Y_MULT
     )
     
     allies[1].goto(destination)

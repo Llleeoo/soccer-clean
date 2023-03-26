8f1979ace46b1cac62bb7cf604b36b68c5ef055a
Llleeoo <120419742+Llleeoo@users.noreply.github.com>
2023-03-26 19:44:50 +0200

    robocup régional
    J'ai tout simplement copié collé nôtre code de la première robocup. Il faudra s'occuper de nettoyer un peu tout le code car on a rajouté pas mal de chose au fur et à mesure de la cup et il y a plein de commentaire inutiles. Au niveau de améliorations à faire il y a le shoot défenseur (on peut utilisé la technique de Mr Raymond pour calculer la distance entre 2 points dans un plan orthonormé), changer l'angle de zizou avant son tir si un défenseur se trouve sur la trajectoire de la balle (tester la colinéarité des vecteurs) et peut-être rajouter des threds...

--- src/main.py
+++ src/main.py
@@ -1,40 +1,95 @@
-from rsk import Client, ClientRobot, constants
+from rsk import Client, constants
+import rsk
 import numpy as np
 from math import sqrt, atan
+#from time import time
 
 # All mesures are in meters
-ROBOT_RADIUS = 0.09
-BALL_RADIUS = 0.02
-TEAM = "green"
-X_MULT = 1 if TEAM == "green" else -1
-DEFAULT_ANGLE = 0 if TEAM == "green" else np.pi
-client = Client()
+COOLDOWN = 1000
+ROBOT_RADIUS = 0.08
+BALL_RADIUS = 0.01
+TEAM = "blue"
+SIDE = 1
+#Vers négatif = 0; vers positif = 1 !!!!!!!!!
+X_MULT = 1 if SIDE == 0 else -1
+DEFAULT_ANGLE = 0 if SIDE == 1 else np.pi
+client = Client(host="192.168.0.100", key="azert")
+
+#last_kick_zero = time()
 
 def main_loop():
-    allies: dict[ClientRobot, ClientRobot] = client.robots[TEAM]
+    #global last_kick_zero
+
+    allies = ()
+    if TEAM == "blue":
+        allies = (client.blue2, client.blue1)
+    else:
+        allies = (client.green2, client.green1)
+
     ball = client.ball
+
     # Defensor move
     allies[0].goto((
-        ((constants.field_width/2)-ROBOT_RADIUS)*X_MULT,
-        max(min(ball[1], -constants.goal_virtual_height/2), constants.goal_virtual_height/2),
+        0.93*X_MULT,
+        max(min(ball[1], 0.30), -0.30),
         DEFAULT_ANGLE
-    ))
-
+    ), wait=False)
+    """
+    print(last_kick_zero)
+    print(time())
+    if last_kick_zero+COOLDOWN < time():
+        allies[0].kick()
+        last_kick_zero = time()
+    """
     # Attacker move
-    # (attention giga thales incoming)
-    # (feat pithagore)
-    # (feat trigonometrie)
-    pose = allies[1].pose
-    pose[0] *= X_MULT
-    rapport = sqrt(ball[0]**2 + ball[1]**2) + ROBOT_RADIUS + BALL_RADIUS
+    """
+    pose = (ball[0]*X_MULT, ball[1])
+    hypotenuse = sqrt((constants.field_length/2 - pose[0])**2 + pose[1]**2)
+    rapport = hypotenuse / (hypotenuse + ROBOT_RADIUS + BALL_RADIUS)
     destination = (
-        ball[0] * rapport * X_MULT,
-        -abs(ball[1] * rapport),
-        atan(ball[1]/ball[0])
+        (constants.field_length - ((constants.field_length/2 - pose[0]) * rapport)) * X_MULT,
+        pose[1] * rapport,
+        atan(((constants.field_length/2 - pose[0])*X_MULT)/pose[1])
     )
-    
-    allies[1].goto(destination)
+    """
+    bpr = (
+            (ball[0]*X_MULT) - (1.83/2),
+            ball[1]
+            )
+    de = bpr[1]
+    eb = bpr[0]
+    db = sqrt(de**2 + eb**2)
+    ab = db + BALL_RADIUS + ROBOT_RADIUS
+    cb = (eb * ab)/db
+    ac = (de*cb)/eb
+    beta = atan(ac/cb)
+    destination = (
+            (-((1.83/2) + cb)*(-X_MULT))-(0.15*-X_MULT),
+            ac,
+            (DEFAULT_ANGLE - beta)*X_MULT
+            )
 
+    pr = allies[1].pose
+    if round(pr[0]*10) == round(destination[0]*10) and round(pr[1]*100) == round(destination[1]*100):
+        allies[1].kick()
+        print("KICK KICK KICK KICK KICK KICK KICK KICK KICK KICK ZIZOUUUUUU !!!")
+
+    """
+    # If destination is in left or right defense area (extended) OR destination exceed y limit, go in front of our goal
+    if (abs(destination[0]) > (constants.field_length/2) - ROBOT_RADIUS - constants.defense_area_length) or (abs(destination[1] > constants.field_weight/2)):
+        destination[0] = -((constants.field_length/2) - ROBOT_RADIUS - constants.defense_area_length)*X_MULT
+        destination[1] = 0
+    """
+
+    allies[1].goto(destination, wait=False)
+
+def safe_main():
+    try:
+        main_loop()
+    except:
+        print("Error when main loop")
 
 if __name__ == "__main__":
-    client.on_update = main_loop
+    while True:
+        safe_main()
+    #client.on_update = safe_main
